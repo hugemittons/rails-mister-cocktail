5357709e35a20d3ba819003aaa4d74e7cc958c3e
Hugh Mitton <hm@hughmitton.com>
2016-11-25 12:28:27 +0100

    image uploader added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :doses, only: [:destroy]
+
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "rum")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "soda water")
+require 'open-uri'
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+file = open(url).read
+json = JSON.parse(file)
+json['drinks'].each do |ingredient|
+ Ingredient.create name: ingredient['strIngredient1']
+end
